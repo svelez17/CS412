4a57176f441525e62d89e29a364d9087f9622ee0
Stephanie Velez <svelez17@bu.edu>
2020-07-01 21:06:21 -0400

    ps7 submission

--- ps7/src/app/movie.service.ts
+++ ps7/src/app/movie.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { omdb } from '../../configs/omdbAPI';
-import {FormControl} from '@angular/forms';
+
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +12,6 @@ export class MovieService {
   getMovie(movietitle: string) {
     console.log(`In getMovie`);
     console.log(`MOVIETITLE =${movietitle}`);
-    // returns an observable
-    // return this.http.get('http://omdbapi.com/?apikey=2b105dde&t=' + movietitle);
     return this.http.post('http://localhost:3000', {movietitle});
   }
 }
